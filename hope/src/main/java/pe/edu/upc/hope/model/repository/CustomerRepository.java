e69fef067d6686616d0909c2acc5ba3813bec1f7
analuelias <83052312+analuelias@users.noreply.github.com>
2021-06-05 02:10:33 -0500

    proyecto v1
    v1.0

---
+++ hope/src/main/java/pe/edu/upc/hope/model/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package pe.edu.upc.hope.model.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.hope.model.entity.Customer;
+
+
+@Repository
+public interface CustomerRepository  extends JpaRepository<Customer , Integer> {
+
+	
+}
