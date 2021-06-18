cce3f56f3b7ca1786a64fda37afb055d908d893b
Sergio Noriega Pérez <55801394+checho78@users.noreply.github.com>
2021-06-17 21:42:31 -0500

    html home

--- hope/src/main/java/pe/edu/upc/hope/controller/FrontController.java
+++ hope/src/main/java/pe/edu/upc/hope/controller/FrontController.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.hope.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +12,12 @@ public class FrontController {
 	@GetMapping("layout")
 	public String layout() {
 		return "layout/layout";
+	}
+	
+	
+	@GetMapping("home")
+	public String home() {
+		return "layout/home";
 	}
 	
 }
