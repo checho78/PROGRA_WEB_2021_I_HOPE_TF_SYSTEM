8480f665f8f6059c7b7e48d01a51077a60a18f72
LENOVO <LENOVO@192.168.1.7>
2021-06-19 13:05:13 -0500

    search controller

--- hope/src/main/java/pe/edu/upc/hope/controller/SearchController.java
+++ hope/src/main/java/pe/edu/upc/hope/controller/SearchController.java
@@ -1,7 +1,47 @@
 package pe.edu.upc.hope.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.edu.upc.hope.model.entity.Pharmacy;
+import pe.edu.upc.hope.model.entity.Category;
+import pe.edu.upc.hope.service.CategoryService;
+import pe.edu.upc.hope.service.PharmacyService;
+
+@Controller
+@RequestMapping("/search")
 public class SearchController {
 
+	@Autowired
+	private PharmacyService pharmacyService;
+	
+	@PostMapping("district")		// POST: /search/pharmacies
+	public String searchDistrict(Model model , @ModelAttribute("employeeSearch") Pharmacy districtSearch) 
+       {
+		System.out.println("LLEGO");
+		List<Pharmacy> district = null;
+		try {
+			district = pharmacyService.findByDistrict(districtSearch.getDistrict()); 
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("district", district);
+		model.addAttribute("districtSearch", districtSearch);
+		return "search/district";
+	}
 	
 }
+
+
+
+
+
 
