98b75b182a3b625a8e161f704c7242494ed7236a
Sergio Noriega Pérez <55801394+checho78@users.noreply.github.com>
2021-07-02 23:33:35 -0500

    dont have an accout¡

--- hope/src/main/java/pe/edu/upc/hope/model/repository/ProductRepository.java
+++ hope/src/main/java/pe/edu/upc/hope/model/repository/ProductRepository.java
@@ -3,19 +3,21 @@ package pe.edu.upc.hope.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.hope.model.entity.Product;
-import pe.edu.upc.hope.model.entity.Reservation;
-
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
 	
-
+	@Query("select p from Product p where p.name like ?1% ")
 	 List<Product> findByName(String name);
+	
 	    List<Product> findByPrice(Double price);
+	
+	    @Query("select p from Product p where p.description like ?1% ")
 	    List<Product> findByDescription(String description);
 
 	    
